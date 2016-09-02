68f2e4a4ec1b76d020891921679408b6ca77898f
Vitor Bertulucci <vitorbertulucci@hotmail.com>
2016-09-02 09:30:12 -0300

    Do some techniques in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,65 +1,88 @@
+# File: users_controller.rb
+# Purpose: Controller of users
+# License: AGPL.
+
+
 class UsersController < ApplicationController
 
-	# New user alloc
+	# Name: new
+	# Objective: instantiates an object of type User
+	# Parameters: none
+	# Return: user object
+
 	def new
-		if !logged_in?
-	 		@user = User.new 
+
+		if (logged_in? == false)
+	 		@user = User.new
+	 		return @user
 		else
-			redirect_to home_path
+			return redirect_to home_path
 		end
+
 	end
-	
-	# Showing user data
+
+	# Name: show
+	# Objective: find an User object to be present
+	# Parameters: identifier of an User
+	# Return: user object
+
 	def show
+
 		@user = User.find(params[:id])
-		if @user != current_user
-			redirect_to home_path
-		end		
+		if (@user == current_user)
+			return @user
+		else
+			return redirect_to home_path
+		end
+
 	end
-	
-	# Creating new user
+
+	# Name: create
+	# Objective: create an user and set it as current user
+	# Parameters: user parameters
+	# Return: redirection to home page
+
 	def create
-	 	@user = User.new(user_params)
 
-
-		if @user.save
-			# when user create his account he is already log in
+	 	@user = User.new(set_user_parameters)
+		if (@user.save)
 			session[:user_id] = @user.id
 			log_in @user
-			redirect_to home_path
 			flash[:notice] = 'Cadastro efetuado com sucesso!'
+			return redirect_to home_path
 		else
-			render :new
+			return render :new
 		end
 
 	end
 
-	# Searcing user to edit
+	# Name: edit
+	# Objective: find an User object to be present
+	# Parameters: identifier of an User
+	# Return: user object
+
 	def edit
+
 		@user = User.find(params[:id])
-		if @user != current_user
-			redirect_to home_path
-		end			
+		if (@user == current_user)
+			return @user
+		else
+			return redirect_to home_path
+		end
+
 	end
 
-	def user_params
-		params[:user].permit(:name, :email, :password, :password_confirmation,:login, :dateBirthday, :gender, :uf_id, :company_id)
-	end
-	
-	def user_params_update
-		params[:user].permit(:name, :dateBirthday, :gender)
-	end
+	# Name: destroy
+	# Objective: destroy an user
+	# Parameters: user identifier
+	# Return: redirection to home page
 
-	def password_params
-		params[:user].permit(:password, :password_confirmation)
-	end
-
-	# Deleting user 
 	def destroy
+
 		session[:user_id] = nil
  		user = User.find(params[:id])
  		user.attaches.delete_all
- 		update_company_user(user.companies)
+ 		delete_user_company_association(user.companies)
  		user.companies.delete_all
  		user.topics.delete_all
  		user.comments.delete_all
@@ -67,29 +90,43 @@ class UsersController < ApplicationController
  		user.evaluations.delete_all
  		user.denunciations.delete_all
  		user.destroy
-	
-		redirect_to home_path
+		return redirect_to home_path
+
 	end
 
-	# Updating user 
-	def update
-    	@user = User.find(params[:id])
-    	if @user.update_attributes(user_params_update)
-    		respond_to do |format| format.html {redirect_to :action => "show",:id => @user.id}
-	    		flash[:notice] = "Perfil atualizado"
-	    		format.js # views/users/update.js.erb
+	# Name: update
+	# Objective: update some user attributes
+	# Parameters: new user attributes descriptions
+	# Return: user object
 
-	    	end	
+	def update
+
+    	@user = User.find(params[:id])
+    	if (@user.update_attributes(update_user_attributes))
+    		respond_to do |format|
+    			format.html{
+    				redirect_to :action => "show",:id => @user.id
+    			}
+	    		flash[:notice] = "Perfil atualizado"
+	    		format.js
+	    	end
+	    	return @user
     	else
       		flash[:notice] = 'Erro ao atualizar o dado!'
     	end
-    	
+
   	end
 
+  	# Name: update_password
+	# Objective: update user password
+	# Parameters: current password, new password and passwod confirmation
+	# Return: a user object
+
   	def update_password
+
   		@user = User.find(params[:id])
-  		if  @user.authenticate(params[:user][:password_older])
-  			if @user.update_attributes(password_params)
+  		if  (@user.authenticate(params[:user][:password_older]))
+  			if (@user.update_attributes(set_new_password))
   				flash[:notice] = 'Senha atualizada com sucesso!'
   			else
   				flash[:notice] = 'Senha de confirmação invalida!'
@@ -97,12 +134,57 @@ class UsersController < ApplicationController
   		else
   			flash[:notice] = 'Senha invalida!'
   		end
-  		redirect_to :action => "show",:id => @user.id
+  		return redirect_to :action => "show", :id => @user.id
+
   	end
 
-  	def update_company_user(companies)
-  		companies.each do |c|
-  			c.update_attributes(:authenticated => false)
-  		end	
+	# Name: delete_user_company_association
+	# Objective: set as false a user's companies authentications
+	# Parameters: user companies
+	# Return: none
+
+  	def delete_user_company_association(companies)
+
+  		companies.each do |company|
+  			company.update_attributes(:authenticated => false)
+  		end
+
   	end
+
+  	# Name: set_user_parameters
+  	# Objective: set on database a user with its parameters
+	# Parameters: user object
+	# Return: none
+
+	private
+
+		def set_user_parameters
+
+			params[:user].permit(:name, :email, :password, :password_confirmation,:login,
+								 :dateBirthday, :gender, :uf_id, :company_id)
+
+		end
+
+		# Name: update_user_attributes
+	  	# Objective: update user attributes
+		# Parameters: user object
+		# Return: none
+
+		def update_user_attributes
+
+			params[:user].permit(:name, :dateBirthday, :gender)
+
+		end
+
+		# Name: set_new_password
+	  # Objective: update user password
+		# Parameters: user object
+		# Return: none
+
+		def set_new_password
+
+			params[:user].permit(:password, :password_confirmation)
+
+		end
+
 end
