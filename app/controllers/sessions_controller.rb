3f6122be51febdddca140a60ce474279d42d4434
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2016-09-17 19:25:05 -0300

    Applied some techniques on sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,32 +1,68 @@
+# Class: sessions_controller.rb.
+# Purpose: This class is designed to control the actions of session within the Avalia Mais.
+# Avalia Mais.
+# FGA - Universidade de Brasíilia UnB.
+
 class SessionsController < ApplicationController
 
+	# Name: new
+  # Objective: this method create a new instance of session on system.
+  # Parameters: don't have parameters.
+  # Return: redirect to home page.
+
 	def new
-		if logged_in?
-			redirect_to home_path
+
+		if(logged_in?)
+			return redirect_to(home_path)
+		else
+			#nothing to do
 		end
+
 	end
+
+	# Name: create
+	# Objective: this method create a new session on system.
+	# Parameters: user params.
+	# Return: render a new page of logged user.
 
 	def create
+
 		user = find_user_record
 
-		if user && user.authenticate(params[:session][:password])
-			log_in user
-			# redirect_to user_show_path(user), notice: 'Login realizado com sucesso'
-			redirect_to home_path, notice: 'Login realizado com sucesso'
-		else 
+		if(user && user.authenticate(params[:session][:password]))
+			log_in(user)
+			flash.now[:notice] = 'Login realizado com sucesso'
+			return redirect_to(home_path)
+		else
 			flash.now[:notice] = 'Login ou senha inválidos'
-			render 'new'
+			return render('new')
 		end
+
 	end
+
+	# Name: destroy
+	# Objective: this method destroy a session instance.
+	# Parameters: user identifier.
+	# Return: redirect to login page.
 
 	def destroy
+
 		session[:user_id] = nil
 		flash.now[:notice] = 'Logout efetuado com sucesso'
-		redirect_to home_path
+
+		return redirect_to(home_path)
+
 	end
 
-	def find_user_record
+	# Name: find_user_to_log
+	# Objective: find user to log in system with your nickname or email.
+	# Parameters: user identifier.
+	# Return: redirect to login page.
+
+	def find_user_to_log
+
 		User.find_by(login: params[:session][:login]) || User.find_by(email: params[:session][:login])
+
 	end
 
 end
