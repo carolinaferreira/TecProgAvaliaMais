ae1d08e6690f194df7902c15e672a0296d20dfbb
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2016-10-09 22:25:09 -0300

    Addes logded log, comments and some techniques

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,10 +13,12 @@ class CommentsController < ApplicationController
 	def create
 
 		comment = Comment.create(set_comment_params)
+		logger.debug('A new comment has been created')
 		assert(@comment.kind_of(Comment), 'The object @comment it could not be instantiated'
     + 'because does not belong to controller')
 
-		return redirect_to Topic.find(params[:comment][:topic_id])
+		return redirect_to(Topic.find(params[:comment][:topic_id]))
+		logger.info('The application has been redirected to the topic in which it the comment belongs')
 
 	end
 
@@ -28,12 +30,15 @@ class CommentsController < ApplicationController
 	def edit
 
 		comment = Comment.find(params[:comment][:comment_id])
+		logger.debug('A comment #{comment.comment_id} is being requested to edit')
 		assert(comment.user_id != nil, 'The attribute user_id of comment object is null')
 
 		edit_comment_params(comment)
+		logger.debug('A comment #{comment.comment_id} has been edited')
 		assert(comment != nil, 'The comment object isnt null')
 
 		return redirect_to(Topic.find(comment.topic_id))
+		logger.info('The application has been redirected to the topic in which it the comment belongs')
 
 	end
 
@@ -45,35 +50,52 @@ class CommentsController < ApplicationController
 	def destroy
 
 		comment = Comment.find(params[:format])
+		logger.debug('A comment #{comment.comment_id} is being requested to destroy')
 		assert(comment.kind_of(Comment), 'The object @comment it could not be instantiated'
     + 'because does not belong to controller')
 
-		topic = comment.topic_id
-		comment.denunciations.delete_all
-		assert(comment.denunciations == 0, 'The attribute denunciations of comment was not deleted')
+		comment.denunciations.delete_all # It should be deleted the complaints because the attribute is foreign key
+		logger.debug('All denunciations of comment #{comment.comment_id} has been deleted')
+		assert(comment.denunciations == 0, 'The denunciations of comment was not deleted')
+
+		# This variable saves the topic id because will then be redirected to topic page after the comment has deleted
+		topic_id_of_comment_destroyed = comment.topic_id
+		logger.info('The id of topic to which belongs the comment was saved')
 
 		comment.destroy
+		logger.debug('The comment #{comment.comment_id} has been deleted')
 		assert(comment == nil, 'The comment object isnt null')
 
-		return redirect_to Topic.find(topic)
+		return redirect_to Topic.find(topic_id_of_comment_destroyed)
+		logger.info('The application has been redirected to the topic in which it the comment belonged')
 
 	end
 
-	# Name: set_comment_params.
+	# Name: edit_comment_params.
 	# Objective: set the parameters of the comments and the parameters that can be edited.
 	# Parameters: comment, new description, description, user identifier and topic identifier.
 	# Return: nothing.
 
 	private
-		def set_comment_params
-
-			params[:comment].permit(:description, :user_id, :topic_id)
-
-		end
 
 		def edit_comment_params(comment)
 
 			comment.update_attributes(:description => params[:comment][:new_description])
+			logger.debug('The params of edit comment has been set')
+
+		end
+
+	# Name: set_comment_params.
+	# Objective: set the parameters of the comments to create.
+	# Parameters: comment, new description, description, user identifier and topic identifier.
+	# Return: nothing.
+
+	private
+
+		def set_comment_params
+
+			params[:comment].permit(:description, :user_id, :topic_id)
+			logger.debug('The params of new comment has been set')
 
 		end
 
