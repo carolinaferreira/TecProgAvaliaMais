c96d1a893dc57413dfb6f2fa240aeaccbd2728e8
Vitor Bertulucci <vitorbertulucci@hotmail.com>
2016-10-26 19:04:52 -0200

    "Techniques applied on questions controllers."

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,34 +1,96 @@
+# Class: questions_controller.rb.
+# Purpose: this class controls all of questions dependences
+# Avalia Mais.
+# FGA - Universidade de Brasíilia UnB.
+
 class QuestionsController < ApplicationController
 
-  def new
-    @question = Question.new
-    @company = Company.find(params[:question][:company_id])
+    # Name:
+	# 	- new
+	# Objective:
+	# 	- instantiates an object of type questions and associate that on a company.
+	# *	*Args* :
+	#  	- company identifier.
+	# * *Returns* :
+	# 	- +@question+ -> question object
+	# 	- +@company+ -> company object
 
-  end
+    def new
 
-  def create
-    @question = Question.new(question_params)
-    
-    if @question.save! 
-     redirect_to @question
+        @question = Question.new
+        @company = Company.find(params[:question][:company_id])
+
+        return @question
+        return @company
+
     end
-  end
 
-  def show
+    # Name:
+	# 	- create
+	# Objective:
+	# 	- creating a question
+	# *	*Args* :
+	#  	- question parameters.
+	# * *Returns* :
+	# 	- +@question+ -> question object
 
-    @question = Question.find(params[:id])  
-  end
+    def create
 
-  def results
-    @question = Question.find(params[:id])
-    @votes = @question.votes
-    @options = @question.option 
+        @question = Question.new(question_params)
 
-  end
+        if @question.save!
+            redirect_to @question
+        else
+            # nothing to do
+        end
+    end
 
-  def question_params
-   	params.require(:question).permit(:title, :company_id, :option_attributes => [:title, :question_id])
+    # Name:
+	# 	- show
+	# Objective:
+	# 	- find a question to be presented
+	# *	*Args* :
+	#  	- question identifier.
+	# * *Returns* :
+	# 	- +@question+ -> question object
 
-  end
+    def show
+
+        @question = Question.find(params[:id])
+
+        return @question
+
+    end
+
+    # Name:
+	# 	- results
+	# Objective:
+	# 	- checking questions votes and options
+	# *	*Args* :
+	#  	- question identifier.
+	# * *Returns* :
+	# 	- +@question+ -> question object
+	# 	- +@votes+ -> question votes
+	# 	- +@options+ -> question options
+
+    def results
+
+        @question = Question.find(params[:id])
+        @votes = @question.votes
+        @options = @question.option
+
+        return @question
+        return @votes
+        return @options
+
+    end
+
+    private
+
+        def question_params
+
+            params.require(:question).permit(:title, :company_id, :option_attributes => [:title, :question_id])
+
+        end
 
 end
